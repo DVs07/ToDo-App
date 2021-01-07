0b7c09b677ade423e60296fa25fa1e4ee39319a3
DVs07 <aldopablo7@hotmail.com>
2021-01-07 02:06:14 -0300

    Unnecessary comments were deleted, to have a more readable file

--- todo.js
+++ todo.js
@@ -18,25 +18,21 @@ function todoMain(){
         //Second input agrega la cateoria del elemento
         secInputEle = document.getElementsByTagName("input")[1];
         button = document.getElementById("addBtn");
-        //ulElement = document.getElementsByTagName("ul")[0];
-        //console.log(inputElement);
+        
         selectCategory = document.getElementById("categoryFilter");
     };
     
     function addListeners(){
         button.addEventListener("click", addEntry, false);
         selectCategory.addEventListener("change", filterEntries, false);
-        //selectCategory.addEventListener("click",defaultCat)
+
     }
     function addEntry(event){
-        //console.log(event.target);
+
         let flag = true;
         //1st
         let inputValue = inputElement.value;
-        //Vulnerabilidad del input, se puede agregar tags desde este y cambiar el DOM
-        //ulElement.innerHTML += `<li> ${inputValue} </li>`//Template literal
-        //Concat
-        //"<li>" + inputValue + "</li>"
+        
         inputElement.value = ""; //Resetea el input(vacio)
         
         //2nd
@@ -83,71 +79,14 @@ function todoMain(){
         //Agrega nuevas categorias
         updateOptionsCategory();
 
-        
-        //Separador
 
-        /* Ahora se muestra la lista en una tabla
-        //Se corrige y todo lo igresado por el input es un elemento de la lista
-        let liElement = document.createElement("li");
-        
-        //Creo un input de tipo checkbox
-        let checkboxELement = document.createElement("input");
-        checkboxELement.type = "checkbox";
-        //Lo agrego a mi lista
-        liElement.appendChild(checkboxELement);
-        //El elemento agregado se lo pone dentro de un span, para crear un evento sobre este
-        let spanElement = document.createElement("span");
-        spanElement.innerText = inputValue + " - " + secInputValue;
-        liElement.appendChild(spanElement);
-
-        //Cambiamos innerText, porque al concantenar el icono de eliminar lo toma como texto y no es lo que queremos. Para eso usamos innerHTML
-        //liElement.innerHTML = inputValue; 
-        //Se 
-        //liElement.addEventListener("click", onClick, false);
-        
-        //Se va a pasar por variables el icono, para hacerlo mas dinamico
-        //+ "<i class='las la-trash-alt'></i> ";
-        //Creo mi etiqueta contenedora del icono
-        let deleteIcon = document.createElement("i");
-        deleteIcon.style.fontSize = "1.5em";
-        deleteIcon.style.color = "red";
-        //spanElement.innerText = "delete";
-        deleteIcon.className= "las la-trash-alt";
-        //Se elimina un elemento de la lista
-        deleteIcon.addEventListener("click", deleteItem, false );
-        
-        //Agrego un elemento al nodo padre
-        liElement.appendChild(deleteIcon);
-        ulElement.appendChild(liElement);
-        */
-        
-        //console.log(inputValue);
-         //Funcion para eliminar
-        /*function deleteItem(){
-            this.parentNode.remove();//Elimina el nodo padre con su hijo li>i
-        }*/
         //Alternativa
     function deleteItem(){
         trElement.remove();
         updateOptionsCategory();
 
     }
-    /* Ahora lo hacemos desde css
-    //Aplico un tachado al un elemento de la lista
-    function onClick(){
-        if(flag)
-        {
-            this.classList.add("strike");
-            //this.style.textDecoration = "line-through";
-            flag = !flag;
-        }
-        else{
-            //this.style.textDecoration = "none";
-            this.classList.remove("strike");
-            flag = !flag;
-        }
-        
-    }*/
+
     function done(){
         trElement.classList.toggle("strike");
     }
@@ -155,20 +94,10 @@ function todoMain(){
     
 
     function filterEntries(event){
-        //console.log("filterEntries running");
+
         let selection = selectCategory. value;
         
-        /*
-        for(let i = 1; i < rows.length; i++){
-            //console.log(rows[i]);
-            let category = rows[i].getElementsByTagName("td")[2].innerText;
-            if(category == selectCategory.value){
-                rows[i].style.display = "";
-            }
-            else{
-                rows[i].style.display = "none";
-            }
-        }*/
+
 
         //Alternativa
         //Categoria muestra todas las tareas
@@ -198,10 +127,6 @@ function todoMain(){
         
     }
 
-    /*function defaultCat(){
-        var cat = document.getElementById("categoryFilter").options[0].disabled = true;
-    }*/
-    
     //Actualiza y agrega opciones de categorias 
     function updateOptionsCategory(){
         //Creo un array para esas nuevsa categorias
@@ -214,11 +139,8 @@ function todoMain(){
             }
             let category = row.getElementsByTagName("td")[2].innerText;
             
-            //No se duplican categorias nuevas
-            //if(!newOptionsCategories.includes(category)){
                 //Agrego al final del array la nueva categoria
                 newOptionsCategories.push(category);
-            //}
             
         });
         console.log(newOptionsCategories);
@@ -235,7 +157,7 @@ function todoMain(){
         newCategory.innerText = DEFAULT_OPTION;
         newCategory.value = DEFAULT_OPTION;
         selectCategory.appendChild(newCategory);
-        //newOptionsCategories.forEach( (option) => {
+
             for(let option of optionsSet){
                 //Agregar nuevas categorias desde del input
                 let newCategory = document.createElement("option");
@@ -245,7 +167,6 @@ function todoMain(){
                 selectCategory.appendChild(newCategory);
             }
             
-        //});
         
     }
 
