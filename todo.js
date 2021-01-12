dded88c5def36c843ff245c772c7bc567e7bb20b
DVs07 <aldopablo7@hotmail.com>
2021-01-12 03:14:40 -0300

    The functions that saved the tasks in the local storage are finished, if a task is deleted it is also eliminated from the LS. The date and time of the task entered was added, its functionality is under development.

--- todo.js
+++ todo.js
@@ -6,7 +6,9 @@ function todoMain(){
     let inputElement,
         secInputEle,
         button,
-        ulElement,
+        //ulElement,
+        dateInput,
+        timeInput,
         selectCategory,
         todoList = [];//Se crea un array, este solo puede contener string, se guardan en el local storage.
     
@@ -21,12 +23,16 @@ function todoMain(){
         //Cargo las tareas desde el local storage
         loadLocalStorage()
         renderRows();
+        //Agrega nuevas categorias
+        updateOptionsCategory();
 
     function getElements(){
         //First input agrega elementos a la lista
         inputElement = document.getElementsByTagName("input")[0];
         //Second input agrega la cateoria del elemento
         secInputEle = document.getElementsByTagName("input")[1];
+        dateInput = document.getElementById("dateInput");
+        timeInput = document.getElementById("timeInput");
         button = document.getElementById("addBtn");
         
         selectCategory = document.getElementById("categoryFilter");
@@ -40,26 +46,39 @@ function todoMain(){
     function addEntry(event){
 
         let flag = true;
-        //1st
+        //Todo input
         let inputValue = inputElement.value;
-        
         inputElement.value = ""; //Resetea el input(vacio)
         
-        //2nd
+        //Category input
         let secInputValue = secInputEle.value;
         secInputEle.value = "";
         
-        displayRow(inputValue,secInputValue);
+        //Date input
+        let dateValue = dateInput.value;
+        dateInput.value = "";
+        
+        //Time input
+        let timeValue = timeInput.value;
+        timeInput = "";
+        
+        let object = {
+            id: _uuid(),//Indica la posición que tiene en el array
+            todo: inputValue, 
+            category: secInputValue,
+            date: dateValue,
+            time: timeValue,
+            done: false//Nos va a servir para eliminarlo del LS
+        };
+
+        displayRow(object);
 
         /* PUSH ITEMS */
-        todoList.push(inputValue);//Se guardan los valores del 1er input en el array
+        todoList.push(object);//Se crea un objeto para almacenar en LS también la categoria 
         
         /*Funciones LOCAL STORAGE*/ 
         //funcion para guardar en local storage
         saveLocalStorage();
-
-        //Agrega nuevas categorias
-        updateOptionsCategory();
 
     }
     
@@ -112,10 +131,10 @@ function todoMain(){
                 newOptionsCategories.push(category);
             
         });
-        console.log(newOptionsCategories);
+        //console.log(newOptionsCategories);
 
         let optionsSet = new Set(newOptionsCategories)
-        console.log(optionsSet);
+        //console.log(optionsSet);
 
         //Vacia las categorias
         selectCategory.innerHTML = "";
@@ -146,19 +165,41 @@ function todoMain(){
     function loadLocalStorage(){
         let retrieved = localStorage.getItem("todoList");
         todoList = JSON.parse(retrieved);
-        console.log(typeof todoList);
+        //console.log(typeof todoList);
         if(todoList == null){
             todoList = [];
         }
     }
+    /* ROWS */ 
     function renderRows(){
-        todoList.forEach((todo) => {
-            displayRow(todo, null);
+        todoList.forEach((todoObject) => {
+            /*let arr = ["manzana","naranja"];
+            console.log(typeof arr);
+            for( let keys in arr){
+                console.log(`${keys} -> ${arr[keys]}`);
+            }*/
+
+            //Accedo al campo de mi objeto
+            //let todoEntry = todoObject.todo;
+            //let todoEntry = todoObject["todo"];
+            //let key = "category";
+            //let todoCategory = todoObject[key];
+
+            displayRow(todoObject);
         });
-        console.log(todoList);
+        //console.log(todoList);
     }
     //Muestro las tareas guardadas en local storage
-    function displayRow(inputValue,secInputValue){
+    function displayRow({todo: inputValue, category: secInputValue, id, date, time, done}){
+        //Asigno los valores ingresados a los campos de mi objeto
+        /* 
+        let inputValue = object.todo;
+        let secInputValue = object.category;
+        */
+        
+        //Alternativa de asignación
+        //let{todo: inputValue, category: secInputValue} = object;
+        
         //Tabla
         let todoTable = document.getElementById("todoTable");
 
@@ -174,7 +215,25 @@ function todoMain(){
         trElement.appendChild(tdFirst);
         tdFirst.classList.add("colorTd");
         tdFirst.classList.add("center");
-        checkboxELement.addEventListener("click", done, false);
+        checkboxELement.addEventListener("click", checkboxClickCallback, false);
+        checkboxELement.dataset.id = id;//Lo uso como indice para saber su posición
+        //Celda  Fecha
+        let tdDate = document.createElement("td");
+        tdDate.innerText = date;
+        let dateObject = new Date(date);
+        let formattedDate = dateObject.toLocaleString("es-AR", {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+        });
+        console.log(formattedDate);
+        trElement.appendChild(tdDate);
+        tdDate.classList.add("colorTd");
+        //Celda Hora
+        let tdTime = document.createElement("td");
+        tdTime.innerText = time;
+        trElement.appendChild(tdTime);
+        tdTime.classList.add("colorTd");
         //Celda To-Do
         let tdSecond = document.createElement("td");
         tdSecond.innerText = inputValue;
@@ -190,21 +249,55 @@ function todoMain(){
         deleteIcon.style.fontSize = "1.5em";
         deleteIcon.className= "las la-trash-alt";
         deleteIcon.addEventListener("click", deleteItem, false );
+        deleteIcon.dataset.id = id;
         let tdFourth = document.createElement("td");
         tdFourth.appendChild(deleteIcon);
         trElement.appendChild(tdFourth);
         tdFourth.classList.add("colorTd");
         tdFourth.classList.add("center");
+        tdFourth.classList.add("eliminar");
         
+        console.log(done);
+        checkboxELement.type = "checkbox";
+        checkboxELement.checked = done;//Chequeo el valor del checkbox(V o F) y según su valor lo muestro al recargar la página.
+        if(done){
+            trElement.classList.add("strike");
+        }else{
+            trElement.classList.remove("strike");
+        }
+
         // Eliminar una tarea -  Alternativa
         function deleteItem(){
             trElement.remove();
             updateOptionsCategory();
+            for( let i = 0; i<todoList.length; i++){
+                if(todoList[i].id == this.dataset.id){
+                    todoList.splice(i,1);//Borro  la tarea del LS
+                }
+                saveLocalStorage();
+            }
         }
         //Funcion hecha, se la tacha
-        function done(){
+        function checkboxClickCallback(){
             trElement.classList.toggle("strike");
+            for( let i = 0; i<todoList.length; i++){
+                if(todoList[i].id == this.dataset.id){
+                    todoList[i].done = this.checked;
+                }
+            }
+            //console.log(this.dataset.id);//Confirmamos que esta hecha la tarea
+            saveLocalStorage();
         }
-
+    }
+    function _uuid() {
+        let d = Date.now();
+        if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+          d += performance.now(); //use high-precision timer if available
+        }
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            let r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
     }
 };
